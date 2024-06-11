22510657fbce23577ca7a429df5e475bf9645d57
cailight03 <tdimaglaba@gmail.com>
2024-06-11 13:46:40 +0800

    lol

--- index.php
+++ index.php
@@ -163,7 +163,7 @@ session_start();
                     <div class="card h-100">
                         <div class="card-header">
                             <span class="me-2"><i class="bi bi-bar-chart-fill"></i></span>
-                            Area Chart Example
+                            Area Chart lol
                         </div>
                         <div class="card-body">
                             <canvas class="chart" width="400" height="200"></canvas>
